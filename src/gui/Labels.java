d72dbd3411aa98cd66286caf46d38ef481c423b2
David R (Get) <davidroblesvargasdrv@gmail.com>
2020-02-22 23:32:20 -0600

    #3
    Avance importante en el procesado de los mensajes y strings de éstos.

--- src/gui/Labels.java
+++ src/gui/Labels.java
@@ -1,7 +1,56 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 
 public class Labels {
+
+    private void Textlabel(String port, String text, boolean own) {
+        Label tlabel = new Label(text);
+
+        if(own){
+            tlabel.setPadding(new Insets(0, 5, 0, 0));
+            tlabel.setAlignment(Pos.BASELINE_RIGHT);
+
+        }else{
+            tlabel.setPadding(new Insets(0, 0, 0, 5));
+            tlabel.setAlignment(Pos.BASELINE_LEFT);
+        }
+
+    }
+
+    public void Textconverter(String message, boolean own){
+
+        int index = 0;
+        String port = "";
+        String text = "";
+
+        while(message.substring(index) != ":"){
+
+            port = port + message.substring(index);
+            index += 1;
+
+        }
+
+        index += 1;
+
+        while(index < message.length()){
+
+            text = text + message.substring(index);
+
+        }
+
+        if(own){
+
+            Textlabel("You:", text, own);
+
+        }else{
+
+            Textlabel(port+":", text, own);
+
+        }
+
+    }
 
 }
